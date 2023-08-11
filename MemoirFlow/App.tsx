db543772e9ce80ed9cb240d3d690a080aaf163b7
mnseok <min.seok@g.skku.edu>
2023-08-12 02:01:55 +0900

    default setting
    app: react-native
    backend: nestjs

---
+++ MemoirFlow/App.tsx
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import {View} from 'react-native';
+import HomeScreen from './screens/HomeScreen';
+
+function App(): JSX.Element {
+  return (
+    <View>
+      <HomeScreen />
+    </View>
+  );
+}
+
+export default App;
---
+++ MemoirFlow/screens/HomeScreen/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Component} from 'react';
+import {Text, View} from 'react-native';
+
+export default class HomeScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>글</Text>
+      </View>
+    );
+  }
+}
